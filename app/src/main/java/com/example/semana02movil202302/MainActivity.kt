e664b8184707b3f1f781d95ad76c9809404acc3e
Luis Chang <luis.chang@outlook.com>
2023-09-09 09:15:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/semana02movil202302/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.semana02movil202302
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    private val tag: String = "INF-STATE"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(tag,"MainActivity-->onCreate" )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"MainActivity-->onStart" )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"MainActivity-->onResume" )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag,"MainActivity-->onPause" )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"MainActivity-->onStop" )
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"MainActivity-->onDestroy" )
+    }
+
+
+}
